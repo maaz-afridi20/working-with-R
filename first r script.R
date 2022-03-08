987f50551caa47b4d3ae5f62bed9093c14ad26cb
maaz-afridi20 <maazafridi.ma47@gmail.com>
2022-03-08 21:15:45 +0500

    Variables And Data types

--- first r script.R
+++ first r script.R
@@ -1,1 +1,58 @@
-print('this is first R script')
+# variables 
+
+# for installing packages we can use install.packages('ggplot2')
+# this will download ggplot2 package.
+
+# examples of variable
+
+a = "mobiles"
+print(a)
+
+a = "samsung"
+print(a)
+
+
+num1 = 3
+print(num1)
+class(num1)
+
+char1 = "Rstudio"
+print(char1)
+class(char1)
+
+logi1 = TRUE
+print(logi1)
+class(logi1)
+
+complx1 = 4 + 3i
+print(complx1)
+class(complx1)
+
+
+b <- 10
+print(b)
+
+numm1 <- 20
+numm2 <- 10
+
+print(numm1/numm2)
+print(numm1*numm2)
+print(numm1+numm2)
+print(numm1-numm2)
+
+
+print(numm1<numm2)
+print(numm1>numm2)
+print(numm1==numm2)
+print(numm1!=numm2)
+
+
+log1 = TRUE
+log2 = FALSE
+print(log1&log2) # and operator
+print(log1|log2) # or operator
+
+
+
+
+
