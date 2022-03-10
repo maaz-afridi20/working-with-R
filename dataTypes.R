5485697fecdd4ecdc7ede44906c28f26d20be42c
maaz-afridi20 <maazafridi.ma47@gmail.com>
2022-03-10 20:36:39 +0500

    Working With Matrices

--- dataTypes.R
+++ dataTypes.R
@@ -1,6 +1,13 @@
 # Data Struc
 # Lists :
 # It is data struc of hetregenous type
+veccy <- c("a","b","c")
+typeof(veccy)
+class(veccy)
+# this can also be  use to find the data type of variable
+# and also class
+
+
 
 li <- list(1, "A", T)
 print(li)
@@ -17,6 +24,44 @@ print(lii[[2]])
 print(lii[[2]][3])
 print(lii[[3]]) # will (T,F,T)
 print(lii[[3]][3])
+
+
+# ------------------------------------
+# examples :
+
+subject_names <- c("John","Elia","Poet")
+temperature <- c(98.1,97.1,101.2)
+flu_status <- c(FALSE, FALSE, TRUE)
+
+temperature[[2]]
+temperature[1:3]
+# --------------------------------
+
+# Matrix::
+# it is 2 dim data struc 
+
+mi <- matrix(c(1,2,3,4,5,6))
+class(mi)
+mi
+
+mi <- matrix(c(1,2,3,4,5,6), ncol = 3, nrow = 2)
+mi
+
+mi <- matrix(c(1,2,3,4,5,6),ncol = 3, nrow = 2, byrow = T)
+mi
+mi[1,2] # will print 2
+mi[2,3]
+
+mii <- matrix(c(1,2,3,4,5,6,7,8,9),ncol = 3, nrow = 3, byrow = T)
+mii
+mii[3,3] = 10
+mii
+
+# ------------------------
+
+
+
+
 
 
 
