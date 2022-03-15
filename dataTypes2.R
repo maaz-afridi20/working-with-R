cc4dd6229ddbf740d41a0abcbc839faf9acc602c
maaz-afridi20 <maazafridi.ma47@gmail.com>
2022-03-15 19:47:25 +0500

    Lists :

--- dataTypes2.R
+++ dataTypes2.R
@@ -31,4 +31,62 @@ print(fruits)
 
 # -------------------------------------
 
+# Lists : 
+
+thislist <- list("apple","banana","cherry")
+print(thislist)
+
+# Accessing List 
+
+print(thislist[1]) # Will print apple
+
+# changing itme value
+
+thislist[1] <- "pomegranate"
+print(thislist)
+
+
+length(thislist) # printing length
+
+# checking if the item exist 
+
+"apple" %in% thislist # will return bool values
+
+
+newlist <- append(thislist,"blossom")
+print(newlist)
+# Will add blossom in the end
+
+
+# if we want to add something in specified
+# index the we can do it by 
+
+newlylist <- append(thislist, "orange", after = 2)
+# this will add orange after 2nd index in (thislist)
+length(thislist)
+
+"orange" %in% newlylist # will check orange in newly list
+
+
+# printing in range
+fruitslist <- list("apple", "banana", "cherry", "orange", "kiwi", "melon", "mango")
+fruitslist[3:5]
+
+
+# loops in lists : 
+
+for (i in fruitslist) {
+  print(i) # will print all the items inside the list
+}
+
+
+listy1 <- list("a", "b", "c")
+listy2 <- list(1,2,3)
+listy3 <- c(listy1,listy2)
+print(listy3)
+listy3[1]
+
+
+
+
 
