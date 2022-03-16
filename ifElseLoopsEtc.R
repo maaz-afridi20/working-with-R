8b6f57b3c2163a85838f01fb6c1546a77331a64f
maaz-afridi20 <maazafridi.ma47@gmail.com>
2022-03-16 21:00:51 +0500

    DECISION MAKING STATEMENTS

---
+++ ifElseLoopsEtc.R
@@ -0,0 +1,30 @@
+# Decision Making Statements : 
+
+
+if(iris$Sepal.Length[1]>4){
+  print("Sepal Length is greater")
+}else{
+  print("Sepal Length Is Smaller")
+}
+
+
+if(iris$Sepal.Length[1]<4){
+  print("sepal length is smaller")
+}else{
+  print("Sepal length is greater")
+}
+
+# -----------------------
+
+
+
+
+
+
+
+
+
+
+
+
+
