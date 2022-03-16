5f2ddd64d945d2664f8f197264e7962180362181
maaz-afridi20 <maazafridi.ma47@gmail.com>
2022-03-16 21:56:44 +0500

    DATA MANIPULATION DPLYR

---
+++ DataManipulationDPLYR.R
@@ -0,0 +1,56 @@
+# DATA MANIPULATION : 
+
+# in order to do data manipulation we will 
+# use dplyr package.
+# in order to do work with library we have to load library
+
+library(dplyr)
+laptops <- read.csv('G:/study materials/python video/csvFiles/laptops.csv')
+View(laptops)
+
+# in order to select some specific thing from
+# csv file then we can use select method 
+# to select something from that file.
+
+laptops %>% select(1,2) -> laptops1_2 # will show first 2 columns 
+View(laptops1_2)
+laptops %>% select(3:6) -> laptops3_6
+View(laptops3_6)
+
+laptops %>% select("Manufacturer","Model.Name","Operating.System") -> bynames
+View(bynames)  
+
+# by this we can obtain data from columns
+# through columns names 
+
+# we can also do that like if we want to print
+# that columns whose name starts with this or that 
+# then we can use below method.
+
+laptops %>% select(starts_with("P")) -> startWithP
+View(startWithP)
+# so now this will find that column that start with P
+
+laptops %>% select(starts_with("M")) -> startwithM
+View(startwithM)
+# so now this will find that column that start with M
+
+
+laptops %>% select(ends_with("e")) -> endwithe
+View(endwithe)
+# this will show that colum that name ends with e
+
+laptops %>% select(ends_with("M")) -> endwithm
+View(endwithm)
+
+
+
+
+
+
+
+
+
+
+
+
