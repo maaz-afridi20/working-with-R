453c87c5a60dbedc62ddcc0b7822b19afd737d43
maaz-afridi20 <maazafridi.ma47@gmail.com>
2022-03-22 16:13:43 +0500

    practice

--- DataVisualization.R
+++ DataVisualization.R
@@ -9,3 +9,42 @@ flights
 
 flights %>% filter(carrier == "AS") -> alaska_flights
 View(alaska_flights)
+
+
+ggplot(data = alaska_flights, mapping = aes(x = dep_delay, y = arr_delay)) + 
+  geom_point()
+    
+# -----  data visualization
+
+ggplot(data = alaska_flights, mapping = aes(x = dep_delay, y = arr_delay)) + 
+  geom_point(alpha = 0.5)
+
+
+ggplot(data = alaska_flights, mapping = aes(x = dep_delay, y = arr_delay)) + 
+  geom_point(alpha = 0.1)
+    
+    
+    
+    
+    
+# geom jitter 
+ggplot(data = alaska_flights, mapping = aes(x = dep_delay, y = arr_delay)) + 
+  geom_jitter(width = 30, height = 30)
+
+
+
+
+ggplot(data = early_january_weather, mapping = aes(x = time_hour, y = temp)) + geom_line()
+ 
+
+early_january_weather <- weather %>% 
+  filter(origin == "EWR" & month == 1 & day <= 15)
+
+
+ggplot(data = early_january_weather, 
+       mapping = aes(x = time_hour, y = temp)) +
+  geom_line()
+
+
+   
+    
